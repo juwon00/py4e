5c6347c78b48bced61bdc7f26579e6f33e7797c6
juone00 <juwon0208@naver.com>
2022-08-15 23:47:40 +0900

    1330

--- hello.py
+++ hello.py
@@ -1,6 +1,10 @@
-print("         ,r\'\"7")
-print("r`-_   ,'  ,/")
-print(" \. \". L_r'")
-print("   `~\/")
-print("      |")
-print("      |")
+a, b = input().split()
+a = int(a)
+b = int(b)
+
+if a == b:
+    print("==")
+elif a > b:
+    print(">")
+elif a < b:
+    print("<")
